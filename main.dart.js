a9748bfee8c48373fbf92d88ca0c9d1a0e7af78b
alejoBarr <alejoBarr@users.noreply.github.com>
2025-11-23 00:15:59 +0000

    deploy: f49b196d5bd07c907b8e121b9531854069e66337

--- main.dart.js
+++ main.dart.js
@@ -21970,7 +21970,7 @@ $iD9:1}
 A.a9G.prototype={
 $1(a){var s=A.cd().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/b5990e5ccc5e325fd24f0746e7d6689bbebc7c65/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/13e658725ddaa270601426d1485636157e38c34c/":s)+a},
 $S:74}
 A.Do.prototype={
 gq(a){var s=this.a
